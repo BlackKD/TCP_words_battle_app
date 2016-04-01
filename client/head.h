9cf80961d1b99f36b75bb9c57d9204c221d97601
bqXuan <494692854@qq.com>
2016-04-01 12:02:14 +0800

    .

--- client/head.h
+++ client/head.h
@@ -20,7 +20,7 @@ pthread_t threadID_to_listengame;
 #define MAXLINE 4096
 
 #define SERV_PORT 1234//目标服务器端口
-#define SERV_ADDR "192.168.108.129"
+#define SERV_ADDR "114.212.191.43"
 
 #define true 1
 #define false 0
